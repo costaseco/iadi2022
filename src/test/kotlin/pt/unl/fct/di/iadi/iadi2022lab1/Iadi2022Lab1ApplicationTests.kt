0224604bab42e0e83dc639608a173911bf8b3a6f
João Costa Seco <jrcs@fct.unl.pt>
2022-09-22 16:17:32 +0100

    Added a new Spring application

---
+++ src/test/kotlin/pt/unl/fct/di/iadi/iadi2022lab1/Iadi2022Lab1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package pt.unl.fct.di.iadi.iadi2022lab1
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Iadi2022Lab1ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
