7b01928920537d51ec8357eb25af209c634ce909
João Costa Seco <jrcs@fct.unl.pt>
2022-09-22 16:55:52 +0100

    Added DTOs to the presentation layer

---
+++ src/main/kotlin/pt/unl/fct/di/iadi/iadi2022lab1/presentation/DTO.kt
@@ -0,0 +1,5 @@
+package pt.unl.fct.di.iadi.iadi2022lab1.presentation
+
+data class greetingRequest(val name:String)
+
+data class greetingResponse(val message: String)
--- src/main/kotlin/pt/unl/fct/di/iadi/iadi2022lab1/presentation/HelloController.kt
+++ src/main/kotlin/pt/unl/fct/di/iadi/iadi2022lab1/presentation/HelloController.kt
@@ -1,10 +1,8 @@
 package pt.unl.fct.di.iadi.iadi2022lab1.presentation
 
-import org.springframework.web.bind.annotation.*
-
 class HelloController : HelloAPI {
 
     override fun helloWorld():String = "Hello! World!"
 
-    override fun sayHello(@RequestBody name:String): String = "Hello, $name"
+    override fun sayHello(greeting: greetingRequest): String ="Hello, $greeting.name"
 }
